5db1505c2a81553be5e85ec9dc3bec5e5d986296
fei_yong <yongfeide123@gmail.com>
2020-02-21 16:56:31 +0800

    fix:代码重构

--- src/env.js
+++ src/env.js
@@ -3,5 +3,5 @@ window.ENV = {
     domain: 'http://canyuegongzi.xyz',
     //domain: 'http://172.19.25.6:8092',
     casDomain: '/user-login-cas-web', //cas服务
-    wgmsBaseWeb: 'http://47.106.104.22:8881',
+    wgmsBaseWeb: 'http://127.0.0.1:8881',
  }
